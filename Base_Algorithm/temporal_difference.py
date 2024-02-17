e355e44ed042aed76ba9df940d0ca1f4d361b602
shaoj22 <935619774@qq.com>
2024-02-17 11:24:39 +0800

    update

--- Base_Algorithm/temporal_difference.py
+++ Base_Algorithm/temporal_difference.py
@@ -1,3 +1,11 @@
+'''
+Author: shaoj22 935619774@qq.com
+Date: 2024-02-17 11:24:05
+LastEditors: shaoj22 935619774@qq.com
+LastEditTime: 2024-02-17 11:24:17
+FilePath: \Reinforcement_Learning\Base_Algorithm\temporal_difference.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 
 
 
