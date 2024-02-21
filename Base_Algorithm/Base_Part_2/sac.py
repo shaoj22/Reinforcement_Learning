89e61184e11eb84499e3983e4af9eedddc4686b8
shaoj22 <935619774@qq.com>
2024-02-21 14:56:19 +0800

    update some rf Base_Algorithm

---
+++ Base_Algorithm/Base_Part_2/sac.py
@@ -0,0 +1,8 @@
+'''
+Author: shaoj22 935619774@qq.com
+Date: 2024-02-21 14:51:59
+LastEditors: shaoj22 935619774@qq.com
+LastEditTime: 2024-02-21 14:54:23
+FilePath: \Reinforcement_Learning\Base_Algorithm\sac.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
