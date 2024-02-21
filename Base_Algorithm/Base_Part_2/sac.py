22396c528a86b15533ca1550477305194efe1fff
shaoj22 <935619774@qq.com>
2024-02-21 17:39:16 +0800

    update RL求解CO的泛化能力较强的范式

--- Base_Algorithm/Base_Part_2/sac.py
+++ Base_Algorithm/Base_Part_2/sac.py
@@ -2,7 +2,7 @@
 Author: shaoj22 935619774@qq.com
 Date: 2024-02-21 14:51:59
 LastEditors: shaoj22 935619774@qq.com
-LastEditTime: 2024-02-21 14:54:23
+LastEditTime: 2024-02-21 16:36:36
 FilePath: \Reinforcement_Learning\Base_Algorithm\sac.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+Description: sac algorithm code
 '''
---
+++ Base_Algorithm/Base_Part_2/trpo.py
@@ -0,0 +1,12 @@
+'''
+Author: shaoj22 935619774@qq.com
+Date: 2024-02-21 14:51:31
+LastEditors: shaoj22 935619774@qq.com
+LastEditTime: 2024-02-21 16:37:45
+FilePath: \Reinforcement_Learning\Base_Algorithm\Base_Part_2\trpo.py
+Description: trpo algorithm code
+'''
+
+
+
+
